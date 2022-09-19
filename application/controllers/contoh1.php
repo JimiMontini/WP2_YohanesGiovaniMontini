2040c01b02a3dc54cfa926aca6716b427581ce7e
JimiMontini <jimimontini45@gmail.com>
2022-09-19 19:40:13 +0700

    menambahkan code ignither

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Jimi Montini
+        Saya merantau
+        Olah raga yang saya sukai adalah
+        NgePUBG sampai jari patah meskipun
+        aplikasinya sudah di hapus";
+    }
+}
