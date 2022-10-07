32622fa7cfcd4a23eb1fb021e26e8f861a63ea30
JimiMontini <jimimontini45@gmail.com>
2022-10-07 15:51:45 +0700

    menambah about

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Jimi</li>
+ <li>Nama Belakang : Montini</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jalan Jalan Dulu </li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Ende</li>
+ </ul>
+ <h4>Kegiatan Favorit</h4>
+ <ul type="square">
+ <li>Main Game</li>
+ <li>Biliard</li>
+ </ul>
+</section>
